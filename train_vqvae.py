0313acbe1f2566cf9a1a306b3b277624a36ab690
harrygcoppock <hgc19@ic.ac.uk>
2020-05-23 14:52:23 +0100

    update to default args

--- train_vqvae.py
+++ train_vqvae.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
     parser.add_argument('--epoch', type=int, default=560)
     parser.add_argument('--lr', type=float, default=3e-4)
     parser.add_argument('--sched', type=str)
-    parser.add_argument('path', type=str, default='None')
+    #parser.add_argument('path', type=str, default='None')
 
     args = parser.parse_args()
 
